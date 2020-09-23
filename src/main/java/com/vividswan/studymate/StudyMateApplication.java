6fff1528b0e457dc6c9ce1e922d4c13e9d5e53d1
Soohwan Park <vividswan@naver.com>
2020-09-23 16:11:48 +0900

    Initial commit

---
+++ src/main/java/com/vividswan/studymate/StudyMateApplication.java
@@ -0,0 +1,13 @@
+package com.vividswan.studymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMateApplication.class, args);
+	}
+
+}
