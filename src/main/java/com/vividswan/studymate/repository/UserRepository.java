d6e046fb386c030b562c1a9e40d55128faa75ccb
Soohwan Park <vividswan@naver.com>
2020-09-25 02:01:15 +0900

    Update UserRepository.java

--- src/main/java/com/vividswan/studymate/repository/UserRepository.java
+++ src/main/java/com/vividswan/studymate/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.vividswan.studymate.repository;
 import com.vividswan.studymate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
