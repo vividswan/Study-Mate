9fae60ca9d46fa7a1fd52b7b76d2d26ab3607a46
Soohwan Park <vividswan@naver.com>
2020-09-26 22:44:08 +0900

    Update User.java

--- src/main/java/com/vividswan/studymate/model/User.java
+++ src/main/java/com/vividswan/studymate/model/User.java
@@ -1,24 +1,36 @@
 package com.vividswan.studymate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
     private String username;
+    @Column(nullable = false, length = 100)
     private String nickname;
+    @Column(nullable = false, length = 50)
     private String email;
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
     private RoleType role;
-    @CreatedDate
+
+    @CreationTimestamp
     private Timestamp createDate;
 }
