ab77ec6da15022f7ef40eabaf350c41d495ac069
Soohwan Park <vividswan@naver.com>
2020-09-28 02:53:41 +0900

    Update TaskController.java

--- src/main/java/com/vividswan/studymate/controller/TaskController.java
+++ src/main/java/com/vividswan/studymate/controller/TaskController.java
@@ -1,12 +1,29 @@
 package com.vividswan.studymate.controller;
 
+import com.vividswan.studymate.model.Task;
+import com.vividswan.studymate.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final TaskService taskService;
+
     @GetMapping("/taskForm")
     public String taskForm(){
         return "task/taskForm";
+    }
+
+    @GetMapping("/taskView")
+    public String taskView(Model model){
+        List<Task> tasks = taskService.findList();
+        model.addAttribute("tasks",tasks);
+        return "task/taskView";
     }
 }
