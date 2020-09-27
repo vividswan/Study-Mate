16cd01839879e080e5c3d907a53e393008a92034
Soohwan Park <vividswan@naver.com>
2020-09-28 02:53:45 +0900

    Update TaskService.java

--- src/main/java/com/vividswan/studymate/service/TaskService.java
+++ src/main/java/com/vividswan/studymate/service/TaskService.java
@@ -1,11 +1,14 @@
 package com.vividswan.studymate.service;
 
 import com.vividswan.studymate.dto.TaskWriteDto;
+import com.vividswan.studymate.model.Task;
 import com.vividswan.studymate.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +19,10 @@ public class TaskService {
     @Transactional
     public void write(TaskWriteDto taskWriteDto){
         taskRepository.save(taskWriteDto.toEntity());
+    }
+
+    @Transactional(readOnly = true)
+    public List<Task> findList() {
+        return taskRepository.findAll();
     }
 }
