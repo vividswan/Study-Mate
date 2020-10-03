2cea934ef1912e274aab664dbd64a95202d31ed4
Soohwan Park <vividswan@naver.com>
2020-10-03 20:28:11 +0900

    Create PrincipalDetails.java

---
+++ src/main/java/com/vividswan/studymate/config/auth/PrincipalDetails.java
@@ -0,0 +1,59 @@
+package com.vividswan.studymate.config.auth;
+
+import com.vividswan.studymate.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collections = new ArrayList<>();
+        collections.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "ROLE_"+user.getRole();
+            }
+        });
+        return collections;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
