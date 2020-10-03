ba190fa7e364ef0a025e89012ae72d5af019ef61
Soohwan Park <vividswan@naver.com>
2020-10-03 20:28:22 +0900

    Update SecurityConfig.java

--- src/main/java/com/vividswan/studymate/config/SecurityConfig.java
+++ src/main/java/com/vividswan/studymate/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.vividswan.studymate.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder pwdEncode(){
+        return new BCryptPasswordEncoder();
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
