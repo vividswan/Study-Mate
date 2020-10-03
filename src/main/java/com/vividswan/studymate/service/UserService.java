cc13470d66b3f8f3c1a6a02d3e4fa48c2ade1ed1
Soohwan Park <vividswan@naver.com>
2020-10-03 20:28:27 +0900

    Update UserService.java

--- src/main/java/com/vividswan/studymate/service/UserService.java
+++ src/main/java/com/vividswan/studymate/service/UserService.java
@@ -1,10 +1,12 @@
 package com.vividswan.studymate.service;
 
 import com.vividswan.studymate.dto.UserJoinDto;
+import com.vividswan.studymate.model.RoleType;
 import com.vividswan.studymate.model.User;
 import com.vividswan.studymate.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -13,10 +15,16 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private final UserRepository userRepository;
 
     @Transactional
     public void join(UserJoinDto userJoinDto) {
+        String rawPassword = userJoinDto.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        userJoinDto.setPassword(encPassword);
+        userJoinDto.setRole(RoleType.USER);
         userRepository.save(userJoinDto.toEntity());
     }
 }
