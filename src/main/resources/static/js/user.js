e5de52fb630a20c3f6c2818a94639a95021dda2e
Soohwan Park <vividswan@naver.com>
2020-10-05 07:20:11 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,28 +2,7 @@ let index ={
     init : function() {
         $("#btn-join").on("click",()=>{
             this.save();
-        }),
-        $("#btn-login").on("click", ()=>{
-            this.login();
         })
-    },
-    login: function (){
-        let data ={
-            username : $("#username").val(),
-            password: $("#password").val(),
-        }
-        $.ajax({
-            type:"POST",
-            url:"/api/loginProc",
-            contentType: "application/json;charset=utf-8",
-            data:JSON.stringify(data)
-        })
-            .done(function (response){
-                alert("로그인에 성공했습니다.");
-                location.href="/";
-            }).fail(function (error){
-                alert(JSON.stringify(error));
-        });
     },
     save : function(){
         let data = {
