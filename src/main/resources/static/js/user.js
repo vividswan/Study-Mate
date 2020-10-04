f9f6c5dddf6626177ebcf9616e5098af3d71e919
Soohwan Park <vividswan@naver.com>
2020-10-04 19:24:40 +0900

    Update user.js

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,6 +2,27 @@ let index ={
     init : function() {
         $("#btn-join").on("click",()=>{
             this.save();
+        }),
+        $("#btn-login").on("click", ()=>{
+            this.login();
+        })
+    },
+    login: function (){
+        let data ={
+            username : $("#username").val(),
+            password: $("#password").val(),
+        }
+        $.ajax({
+            type:"POST",
+            url:"/api/loginProc",
+            contentType: "application/json;charset=utf-8",
+            data:JSON.stringify(data)
+        })
+            .done(function (response){
+                alert("로그인에 성공했습니다.");
+                location.href="/";
+            }).fail(function (error){
+                alert(JSON.stringify(error));
         });
     },
     save : function(){
@@ -21,10 +42,10 @@ let index ={
         })
         .done(function(response){
             if(response.status === 500){
-                alert("로그인에 실패하였습니다.");
+                alert("회원 가입에 실패하였습니다.");
             }
             else{
-                alert("로그인에 성공하였습니다.");
+                alert("회원 가입에 성공하였습니다.");
             }
             location.href="/";
         }).fail(function (error){
