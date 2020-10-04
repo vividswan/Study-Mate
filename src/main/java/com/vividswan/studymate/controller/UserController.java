87c1b377ba46624fc8943d61590bf2f56145db1c
Soohwan Park <vividswan@naver.com>
2020-10-05 07:19:50 +0900

    Update UserController.java

--- src/main/java/com/vividswan/studymate/controller/UserController.java
+++ src/main/java/com/vividswan/studymate/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
@@ -18,4 +20,9 @@ public class UserController {
         return "user/loginForm";
     }
 
+    @ResponseBody
+    @GetMapping("userInfo")
+    public String userInfo(){
+        return "userInfo";
+    }
 }
