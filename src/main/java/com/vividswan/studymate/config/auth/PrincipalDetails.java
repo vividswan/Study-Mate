7398e2a636b8ecd93dea260dd35e074bcbce3f8f
Soohwan Park <vividswan@naver.com>
2020-10-04 22:19:51 +0900

    Update PrincipalDetails.java

--- src/main/java/com/vividswan/studymate/config/auth/PrincipalDetails.java
+++ src/main/java/com/vividswan/studymate/config/auth/PrincipalDetails.java
@@ -56,4 +56,8 @@ public class PrincipalDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getUserId() {
+        return user.getId();
+    }
 }
