0134027d1f52470f1189aeba4498a934f7be2d67
Soohwan Park <vividswan@naver.com>
2020-10-05 14:32:46 +0900

    Update UserController.java

--- src/main/java/com/vividswan/studymate/controller/UserController.java
+++ src/main/java/com/vividswan/studymate/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.vividswan.studymate.controller;
 
+import com.vividswan.studymate.config.auth.PrincipalDetails;
 import com.vividswan.studymate.dto.UserJoinDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,9 +23,11 @@ public class UserController {
         return "user/loginForm";
     }
 
-    @ResponseBody
     @GetMapping("userInfo")
-    public String userInfo(){
-        return "userInfo";
+    public String userInfo(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        model.addAttribute("username",principalDetails.getUsername());
+        model.addAttribute("email",principalDetails.getEmail());
+        model.addAttribute("nickname",principalDetails.getNickname());
+        return "/user/userInfo";
     }
 }
