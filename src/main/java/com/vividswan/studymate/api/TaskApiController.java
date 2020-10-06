ad9b87371e731ad6cff98e6c47205ceb9c58bb7a
Soohwan Park <vividswan@naver.com>
2020-10-07 07:50:05 +0900

    Update TaskApiController.java

--- src/main/java/com/vividswan/studymate/api/TaskApiController.java
+++ src/main/java/com/vividswan/studymate/api/TaskApiController.java
@@ -1,10 +1,12 @@
 package com.vividswan.studymate.api;
 
+import com.vividswan.studymate.config.auth.PrincipalDetails;
 import com.vividswan.studymate.dto.TaskWriteDto;
 import com.vividswan.studymate.model.Task;
 import com.vividswan.studymate.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,8 @@ public class TaskApiController {
     private final TaskService taskService;
 
     @PostMapping("api/writeProc")
-    public int write(@RequestBody TaskWriteDto taskWriteDto){
-        taskService.write(taskWriteDto);
+    public int write(@RequestBody TaskWriteDto taskWriteDto, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        taskService.write(taskWriteDto, principalDetails);
         return HttpStatus.OK.value();
     }
 }
