b2c33d5f98e8bc088b01a51dd93b565f8e63f8fe
Soohwan Park <vividswan@naver.com>
2020-10-07 07:50:18 +0900

    Update TaskRepository.java

--- src/main/java/com/vividswan/studymate/repository/TaskRepository.java
+++ src/main/java/com/vividswan/studymate/repository/TaskRepository.java
@@ -3,5 +3,9 @@ package com.vividswan.studymate.repository;
 import com.vividswan.studymate.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task ,Long> {
+    List<Task> findAllByUserId(Long userId);
 }
