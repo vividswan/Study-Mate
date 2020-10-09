0ee22cc54a96ea0ae3426ed7171bed76e024b27d
Soohwan Park <vividswan@naver.com>
2020-10-09 17:37:26 +0900

    Update TaskController.java

--- src/main/java/com/vividswan/studymate/controller/TaskController.java
+++ src/main/java/com/vividswan/studymate/controller/TaskController.java
@@ -4,10 +4,15 @@ import com.vividswan.studymate.config.auth.PrincipalDetails;
 import com.vividswan.studymate.model.Task;
 import com.vividswan.studymate.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,13 +32,15 @@ public class TaskController {
     }
 
     @GetMapping("/todolist/proceeding")
-    public String taskView(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
-        List<Task> tasks = taskService.findList(principalDetails);
-        String username =principalDetails.getUsername();
-        String nickname = principalDetails.getNickname();
-        model.addAttribute("tasks",tasks);
-        model.addAttribute("username",username);
-        model.addAttribute("nickname",nickname);
-        return "task/taskView";
+    @ResponseBody
+    public Page<Task> taskView(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails, @PageableDefault(size=8, sort = "deadline", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<Task> tasks = taskService.findList(principalDetails, pageable);
+        return tasks;
+//        String username =principalDetails.getUsername();
+//        String nickname = principalDetails.getNickname();
+//        model.addAttribute("tasks",tasks);
+//        model.addAttribute("username",username);
+//        model.addAttribute("nickname",nickname);
+//        return "task/taskView";
     }
 }
