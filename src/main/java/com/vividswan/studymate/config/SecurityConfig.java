51e4271cf52bac68c04d51b1060dba3d7b0c7aa6
Soohwan Park <vividswan@naver.com>
2020-10-09 16:21:08 +0900

    Update SecurityConfig.java

--- src/main/java/com/vividswan/studymate/config/SecurityConfig.java
+++ src/main/java/com/vividswan/studymate/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/loginForm?first=0")
+                .loginPage("/loginForm/0")
                 .loginProcessingUrl("/api/loginProc")
                 .defaultSuccessUrl("/");
     }
