df937171dae5fee96e581b5c73f91772c38cf4ab
Soohwan Park <vividswan@naver.com>
2020-10-09 16:21:04 +0900

    Update UserController.java

--- src/main/java/com/vividswan/studymate/controller/UserController.java
+++ src/main/java/com/vividswan/studymate/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
@@ -13,8 +14,8 @@ public class UserController {
         return "user/joinForm";
     }
 
-    @GetMapping("loginForm")
-    public String loginForm(Model model, int first)
+    @GetMapping("loginForm/{first}")
+    public String loginForm(Model model, @PathVariable int first)
     {
         boolean chk;
         if(first==1){
