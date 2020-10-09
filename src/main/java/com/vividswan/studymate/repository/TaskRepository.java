e0fa78c850efbc357aeef7c80c176a7f54ce3403
Soohwan Park <vividswan@naver.com>
2020-10-09 17:36:54 +0900

    Update TaskRepository.java

--- src/main/java/com/vividswan/studymate/repository/TaskRepository.java
+++ src/main/java/com/vividswan/studymate/repository/TaskRepository.java
@@ -1,11 +1,13 @@
 package com.vividswan.studymate.repository;
 
 import com.vividswan.studymate.model.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task ,Long> {
-    List<Task> findAllByUserIdAndIsSuccess(Long userId,int isSuccess);
+    Page<Task> findAllByUserIdAndIsSuccess(Long userId, int isSuccess, Pageable pageable);
 }
