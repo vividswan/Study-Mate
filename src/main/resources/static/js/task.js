b00439b22fa93e9a576354f2aba1545e537add94
Soohwan Park <vividswan@naver.com>
2020-10-13 15:23:48 +0900

    Update task.js

--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -2,7 +2,33 @@ let index = {
     init: function (){
         $("#btn-write").on("click", ()=>{
             this.save();
+        });
+        $("#btn-delete").on("click", ()=>{
+            this.delete();
         })
+    },
+    delete: function (){
+       let data = {
+           id: $("#id").val()
+       }
+       $.ajax({
+           type:"DELETE",
+           url:`/api/taskDelete/${data.id}`,
+           contentType: "application/json;utf-8"
+       })
+           .done(function (response){
+               if(response===500){
+                   alert("삭제에 실패했습니다.");
+               }
+               else{
+                   alert("삭제에 성공했습니다.");
+               }
+               history.back();
+           })
+           .fail(function (error){
+               alert(JSON.stringify(error));
+           })
+
     },
     save: function (){
         let data = {
