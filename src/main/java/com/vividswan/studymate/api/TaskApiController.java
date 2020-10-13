c676b0e3fae981d161a2017a4f284f9785d4f8fc
Soohwan Park <vividswan@naver.com>
2020-10-13 21:10:14 +0900

    Update TaskApiController.java

--- src/main/java/com/vividswan/studymate/api/TaskApiController.java
+++ src/main/java/com/vividswan/studymate/api/TaskApiController.java
@@ -1,6 +1,7 @@
 package com.vividswan.studymate.api;
 
 import com.vividswan.studymate.config.auth.PrincipalDetails;
+import com.vividswan.studymate.dto.TaskUpdateDto;
 import com.vividswan.studymate.dto.TaskWriteDto;
 import com.vividswan.studymate.model.Task;
 import com.vividswan.studymate.service.TaskService;
@@ -24,6 +25,12 @@ public class TaskApiController {
     @DeleteMapping("/api/taskDelete/{id}")
     public int delete(@PathVariable long id){
         taskService.delete(id);
+        return HttpStatus.OK.value();
+    }
+
+    @PutMapping("/api/taskUpdate/{id}")
+    public int update(@PathVariable long id, @AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody TaskUpdateDto taskUpdateDto){
+        taskService.update(id, taskUpdateDto);
         return HttpStatus.OK.value();
     }
 }
