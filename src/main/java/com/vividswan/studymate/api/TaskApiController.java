6be529c5d83a546f5ded13d7ae9be48695f4953f
Soohwan Park <vividswan@naver.com>
2020-10-13 15:24:06 +0900

    Update TaskApiController.java

--- src/main/java/com/vividswan/studymate/api/TaskApiController.java
+++ src/main/java/com/vividswan/studymate/api/TaskApiController.java
@@ -7,9 +7,7 @@ import com.vividswan.studymate.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,12 @@ public class TaskApiController {
     @PostMapping("api/writeProc")
     public int write(@RequestBody TaskWriteDto taskWriteDto, @AuthenticationPrincipal PrincipalDetails principalDetails){
         taskService.write(taskWriteDto, principalDetails);
+        return HttpStatus.OK.value();
+    }
+
+    @DeleteMapping("/api/taskDelete/{id}")
+    public int delete(@PathVariable long id){
+        taskService.delete(id);
         return HttpStatus.OK.value();
     }
 }
