d9bbb23d485688660cd4a1453559b2d349caad54
Soohwan Park <vividswan@naver.com>
2020-10-24 23:13:45 +0900

    Create feedbackApiController.java

---
+++ src/main/java/com/vividswan/studymate/api/feedbackApiController.java
@@ -0,0 +1,23 @@
+package com.vividswan.studymate.api;
+
+import com.vividswan.studymate.dto.FeedbackCreateDto;
+import com.vividswan.studymate.service.FeedbackService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class feedbackApiController {
+
+    private final FeedbackService feedbackService;
+
+
+    @PostMapping("/api/task/create")
+    int createFeedback(@RequestBody FeedbackCreateDto feedbackCreateDto){
+        feedbackService.createFeedback(feedbackCreateDto);
+        return HttpStatus.OK.value();
+    }
+}
