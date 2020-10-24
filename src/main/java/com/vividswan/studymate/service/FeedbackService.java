5f0bc426ff0fb2e93f9cb03b438d3b412d38b0a3
Soohwan Park <vividswan@naver.com>
2020-10-24 23:11:15 +0900

    Create FeedbackService.java

---
+++ src/main/java/com/vividswan/studymate/service/FeedbackService.java
@@ -0,0 +1,19 @@
+package com.vividswan.studymate.service;
+
+import com.vividswan.studymate.dto.FeedbackCreateDto;
+import com.vividswan.studymate.repository.FeedbackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class FeedbackService {
+
+    private final FeedbackRepository feedbackRepository;
+
+    @Transactional
+    public void createFeedback(FeedbackCreateDto feedbackCreateDto) {
+        feedbackRepository.createReply(feedbackCreateDto.getUserId(), feedbackCreateDto.getUserId(), feedbackCreateDto.getContent());
+    }
+}
