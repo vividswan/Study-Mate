cde5dd0df6f71c6e568d31b64d92e31f8e631814
Soohwan Park <vividswan@naver.com>
2020-10-25 01:51:33 +0900

    Update feedbackApiController.java

--- src/main/java/com/vividswan/studymate/api/feedbackApiController.java
+++ src/main/java/com/vividswan/studymate/api/feedbackApiController.java
@@ -4,9 +4,7 @@ import com.vividswan.studymate.dto.FeedbackCreateDto;
 import com.vividswan.studymate.service.FeedbackService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,9 +12,14 @@ public class FeedbackApiController {
 
     private final FeedbackService feedbackService;
 
+    @DeleteMapping("/api/feedback/delete/{id}")
+    public void deleteFeedback(@PathVariable long id){
+        System.out.println(id);
+        feedbackService.deleteFeedback(id);
+    }
 
     @PostMapping("/api/task/create")
-    int createFeedback(@RequestBody FeedbackCreateDto feedbackCreateDto){
+    public int createFeedback(@RequestBody FeedbackCreateDto feedbackCreateDto){
        feedbackService.createFeedback(feedbackCreateDto);
         return HttpStatus.OK.value();
     }
