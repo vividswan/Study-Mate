c8216d48a11a9ce45462d508409f491b908a3a23
Soohwan Park <vividswan@naver.com>
2020-10-24 19:13:46 +0900

    Update FeedbackRepository.java

--- src/main/java/com/vividswan/studymate/repository/FeedbackRepository.java
+++ src/main/java/com/vividswan/studymate/repository/FeedbackRepository.java
@@ -2,6 +2,14 @@ package com.vividswan.studymate.repository;
 
 import com.vividswan.studymate.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+
+    @Modifying
+    @Query(value = "INSERT INTO reply(userId, taskId, content, createDate) VALUES(?1, ?2, ?3, now())",nativeQuery = true)
+    long createReply(long userId, long boardId, String content);
+
 }
