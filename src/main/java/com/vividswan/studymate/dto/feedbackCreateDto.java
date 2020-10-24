aa6719615991488546dd450d4b31e3b425b702c2
Soohwan Park <vividswan@naver.com>
2020-10-25 01:14:48 +0900

    Update feedbackCreateDto.java

--- src/main/java/com/vividswan/studymate/dto/feedbackCreateDto.java
+++ src/main/java/com/vividswan/studymate/dto/feedbackCreateDto.java
@@ -1,18 +1,12 @@
 package com.vividswan.studymate.dto;
 
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class FeedbackCreateDto {
     private long userId;
     private long taskId;
     private String content;
-
-    @Builder
-    FeedbackCreateDto(long userId, long taskId, String content){
-        this.userId = userId;
-        this.taskId = taskId;
-        this.content = content;
-    }
 }
