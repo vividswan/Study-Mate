d9f39243414f6d5bc3f1084e5c7209f0efe9fc7e
vividswan <vividswan@naver.com>
2020-11-26 16:22:11 +0900

    Update IndexController.java

--- src/main/java/com/vividswan/studymate/controller/IndexController.java
+++ src/main/java/com/vividswan/studymate/controller/IndexController.java
@@ -1,13 +1,11 @@
 package com.vividswan.studymate.controller;
 
 import com.vividswan.studymate.config.auth.PrincipalDetails;
-import com.vividswan.studymate.model.User;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
 
 @Controller
 public class IndexController {
